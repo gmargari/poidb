4735c9ff934e9fb763d376d42a0966ab9683b319
Giorgos Margaritis <gmargari@cs.uoi.gr>
2016-01-12 16:23:17 +0200

    Added some sample POIs for thessaloniki and an importing python script
    To import sample POIs:
    ./transform_json_for_mongoimport.py sample_POIs.json | mongoimport --db poidb --collection pois

---
+++ sample_points/transform_json_for_mongoimport.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import json
+import sys
+import os
+
+if (len(sys.argv) != 2):
+    print "Syntax: " + sys.argv[0] + " <sample_json_file>"
+    sys.exit(1)
+
+if (not os.path.isfile(sys.argv[1])):
+    print "Error: file '" + sys.argv[1] + "' does not exist."
+    sys.exit(1)
+
+with open(sys.argv[1]) as data_file:
+    json_objects = json.load(data_file)
+
+    for oid in json_objects:
+        doc = json_objects[oid]
+        objid = doc["oid"]
+        lat = float(doc["latitude"])
+        lon = float(doc["longitude"])
+        doc["location"]  = { "coordinates" : [ lon, lat ], "type" : "Point" }
+        doc["_id"] = { '$oid' : objid }
+        del doc["latitude"]
+        del doc["longitude"]
+        del doc["oid"]
+
+        print json.dumps(doc)
